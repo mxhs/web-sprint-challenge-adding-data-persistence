0720f274cbc737a717de3545f058d3914b0f86bf
mxhs <maxwellstofman@gmail.com>
2021-01-22 08:52:21 -0800

    Test

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,20 @@
-// do not make changes to this file (except to optionally add seeds)
+// do nott make changes to this file (except to optionally add seeds)
 const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-}
+	client: "sqlite3",
+	useNullAsDefault: true,
+	migrations: { directory: "./data/migrations" },
+	pool: {
+		afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+	},
+};
 
 module.exports = {
-  development: {
-    ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
-  },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/test.db3' },
-  },
+	development: {
+		...sharedConfig,
+		connection: { filename: "./data/lambda.db3" },
+	},
+	testing: {
+		...sharedConfig,
+		connection: { filename: "./data/test.db3" },
+	},
 };
